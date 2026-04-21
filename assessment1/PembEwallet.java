df8e248457798681118d571d6ea1ddd36da54c59
Nouzn_nou <daff.kuncoro@gmail.com>
2026-04-21 23:26:16 +0700

    feat: implement PembEWallet class extending Pembayaran for e-wallet transactions

--- assessment1/PembEwallet.java
+++ assessment1/PembEwallet.java
@@ -1,11 +1,11 @@
-public class PembEwallet extends Pembayaran {
+public class PembEWallet extends Pembayaran {
     private String eWallet;
 
     public PembEWallet(int id, String nama, String nominal, String eWallet) {
         super(id, nama, nominal);
         this.eWallet = eWallet;
     }
-    
+
     public String getEWallet() {
         return eWallet;
 
