385acfa21c1fc871a178cac274eefeda43118b99
Nouzn_nou <daff.kuncoro@gmail.com>
2026-04-21 23:02:09 +0700

    add PembEwallet

---
+++ assessment1/PembEwallet.java
@@ -0,0 +1,13 @@
+public class PembEwallet extends Pembayaran {
+    private String eWallet;
+
+    public PembEWallet(int id, String nama, String nominal, String eWallet) {
+        super(id, nama, nominal);
+        this.eWallet = eWallet;
+    }
+    
+    public String getEWallet() {
+        return eWallet;
+
+    }
+}
